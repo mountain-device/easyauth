1031055ac33ea8e4d6207bb707753a938828ca39
Carl Goldberg <carlgoldberg1236@gmail.com>
2014-10-27 10:49:40 -0700

    Includes jquery.min.map in bowercopy in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -97,6 +97,7 @@ module.exports = function(grunt) {
         },
         files: {
           'jquery.min.js': 'jquery/jquery.min.js',
+          'jquery.min.map': 'jquery/jquery.min.map',
           'underscore-min.map': 'underscore/underscore-min.map',
           'underscore-min.js': 'underscore/underscore-min.js',
           'bootstrap.min.css': 'bootstrap/dist/css/bootstrap.min.css'
